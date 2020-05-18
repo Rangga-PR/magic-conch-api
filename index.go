162f99a83d34a18cabd49fb6186b8080e7418f0b
Rangga-PR <ranggaputraraharja@gmail.com>
2020-05-19 06:15:34 +0700

    implement ask question

--- index.go
+++ index.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"math/rand"
+	"net/http"
 	"time"
 )
+
+type Request struct {
+	Question string `json:"question"`
+}
+
+type Response struct {
+	Answer string `json:"answer"`
+}
 
 func generateAnswer() string {
 	answers := []string{
@@ -17,6 +28,32 @@ func generateAnswer() string {
 	return answers[rand.Intn(len(answers))]
 }
 
+func askMagicConch(w http.ResponseWriter, r *http.Request) {
+
+	var req Request
+	defer r.Body.Close()
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	answer := Response{Answer: generateAnswer()}
+	var buffer bytes.Buffer
+	err = json.NewEncoder(&buffer).Encode(&answer)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("content-type", "application/json")
+	w.Write([]byte(buffer.String()))
+}
+
 func main() {
-	println(generateAnswer())
+	http.HandleFunc("/", askMagicConch)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
 }
